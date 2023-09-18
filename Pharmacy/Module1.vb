cd6dd7fe5c275314f825ecdbf00b64b8d14c755d
Saleem <nellaibill@gmail.com>
2023-09-18 15:57:45 +0530

    Modified:17Sep2023-LastChanges

--- Pharmacy/Module1.vb
+++ Pharmacy/Module1.vb
@@ -1,8 +1,8 @@
 Imports System.Data.SqlClient
 Module Module1
-    Public con As New SqlConnection("Data Source=insure\SQLEXPRESS;Initial Catalog=PharmacyDB;Integrated Security=True")
+    Public con As New SqlConnection("Data Source=192.168.1.13;Initial Catalog=PharmacyDB;Persist Security Info=True;User ID=user1;Password=user11")
     'Public con As New SqlConnection("Data Source=SAM-PC\SQLExpress;Initial Catalog=PharmacyDB;Integrated Security=True")
-    Public con1 As New SqlConnection("Data Source=SERVER\SQLEXPRESS;Initial Catalog=LaskhmiHospital;Persist Security Info=True;User ID=sa")
+    Public con1 As New SqlConnection("Data Source=SERVER\SQLEXPRESS;Initial Catalog=LaskhmiHospital;Persist Security Info=True;User ID=sa;Password=''")
     'Public con1 As New SqlConnection("Data Source=SAM-PC\SQLExpress;Initial Catalog=LakshmiHospitalDB;Integrated Security=True")
     Public UserRights, UserName As String
     Public crysBillNo, crysBillYear As String
