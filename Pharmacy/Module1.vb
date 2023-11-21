ced19e69c861c8630b5a5fa6c45b2cd8275a4ab8
INSURE\coolm <laks.hospital@gmail.com>
2023-11-21 17:38:11 +0530

    30-oct-2023-changes-connection string

--- Pharmacy/Module1.vb
+++ Pharmacy/Module1.vb
@@ -1,6 +1,6 @@
 Imports System.Data.SqlClient
 Module Module1
-    Public con As New SqlConnection("Data Source=192.168.1.13;Initial Catalog=PharmacyDB;Persist Security Info=True;User ID=user1;Password=user11")
+    Public con As New SqlConnection("Data Source=192.168.1.13;Initial Catalog=PharmacyDB;Persist Security Info=True;User ID=lhs1;Password=lhs1")
     'Public con As New SqlConnection("Data Source=SAM-PC\SQLExpress;Initial Catalog=PharmacyDB;Integrated Security=True")
     Public con1 As New SqlConnection("Data Source=SERVER\SQLEXPRESS;Initial Catalog=LaskhmiHospital;Persist Security Info=True;User ID=sa;Password=''")
     'Public con1 As New SqlConnection("Data Source=SAM-PC\SQLExpress;Initial Catalog=LakshmiHospitalDB;Integrated Security=True")
